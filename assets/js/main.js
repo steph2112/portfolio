431d90b2c281e4bb36065b3a695dc7de06fb45eb
miguel-uzcategui <m.angel011192@gmail.com>
2025-07-10 22:39:27 +0200

    improve styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -127,14 +127,14 @@
   });
 
 
-  // Init AOS
-  function aos_init() {
-    AOS.init({
-      duration: 1000,
-      once: true,
-    });
-  }
+//  Init AOS
+  // function aos_init() {
+  //   AOS.init({
+  //     duration: 1000,
+  //     once: true,
+  //   });
+  // }
   $(window).on("load", function () {
-    aos_init();
+    // aos_init();
   });
 })(jQuery);
