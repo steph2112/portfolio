7e7eea2fdec3ddb7502216b73b3f292cbd8319cb
miguel-uzcategui <m.angel011192@gmail.com>
2025-07-14 18:33:53 +0200

    improve the loader

--- assets/js/main.js
+++ assets/js/main.js
@@ -125,16 +125,4 @@
     );
     return false;
   });
-
-
-//  Init AOS
-  // function aos_init() {
-  //   AOS.init({
-  //     duration: 1000,
-  //     once: true,
-  //   });
-  // }
-  $(window).on("load", function () {
-    // aos_init();
-  });
 })(jQuery);
